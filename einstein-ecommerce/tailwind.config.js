95c920865e75956fd8aa0d999ddddf835a60efba
Edrei <edreimateus@gmail.com>
2024-08-26 17:49:57 -0300

    style: add project tailwind theme

--- einstein-ecommerce/tailwind.config.js
+++ einstein-ecommerce/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          dark: '#0E1422',
+          light: '#071D3D'
+        },
+        secondary: {
+          dark: '#F6F6F6',
+          light: '#FFFFFF'
+        },
+        terciary: {
+          dark: '#5C5F6A',
+          light: '#878A92',
+          lightest: '#E6E7E8'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
